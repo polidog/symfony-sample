df59a0d4751122e67f27de2dd3fbb01f3a345de3
yoshinyan <hai_hai_hai_1229@yahoo.co.jp>
2023-01-25 23:33:41 +0900

    csv dll

--- src/Controller/BookMyListController.php
+++ src/Controller/BookMyListController.php
@@ -9,21 +9,59 @@ use App\Entity\BookMyList;
 
 class BookMyListController extends AbstractController
 {
+    const HEADER = [
+        'id',
+        'ISBN',
+        'book_title',
+        'author',
+        'created_at'
+    ];
+
+    const FILE_NAME = 'book_list.csv';
+
     #[Route('/book_my_list/page/{page}', name: 'book_my_list/page')]
     public function page($page = 1): Response
     {
-        $limit = 1;
+        $limit = 9;
         $repository = $this->getDoctrine()->getRepository(BookMyList::class);
         $paginator = $repository->getPage($page, $limit);
-        $maxPages = ceil($paginator->count() / $limit);
+        $num_of_books = $paginator->count();
 
+        $maxPages = ceil($num_of_books / $limit);
+        $empty_row_len = $limit - $num_of_books;
         return $this->render('book_my_list/index.html.twig', [
             'title' => '持っている本一覧',
             'book_list' => $paginator->getIterator(),
             'maxPages' => $maxPages,
             'thisPage' => $page,
-//            'empty_row_len' => $empty_row_len
-            'empty_row_len' => 5
+            'num_of_books' => $num_of_books,
+            'empty_row_len' => $empty_row_len
         ]);
     }
+
+    #[Route('/book_my_list/csv_download', name: 'csv_download')]
+    public function csvDownload()
+    {
+        $contents = $this->getDoctrine()->getRepository(BookMyList::class)->findAll();
+
+        $fp = fopen('php://output', 'w');
+
+        fputcsv($fp, BookMyListController::HEADER);
+        foreach ($contents as $row) {
+            fputcsv($fp, [
+                $row->getId(),
+                $row->getIsbn(),
+                $row->getBookTitle(),
+                $row->getAuthor(),
+                $row->getCreatedAt()->format('Y-m-d H:i:s')
+            ], ',', '"');
+        }
+        fclose($fp);
+
+        header('Content-Type: application/octet-stream');
+        header("Content-Disposition: attachment; filename=" . BookMyListController::FILE_NAME);
+        header('Content-Transfer-Encoding: binary');
+        exit;
+    }
+
 }
